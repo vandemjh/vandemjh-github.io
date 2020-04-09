e808dd65d2d1c2cd61c59f26cf8af137dd99327f
vandemjh <jack.vandem@gmail.com>
2020-04-09 16:04:40 -0400

    updated with more spacing between cubes and new random colors for each face

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var camera, scene, renderer;
 var geometry, material;
 var cubes = [];
 
-const boxDimensions = 0.01;
+const boxDimensions = 0.2;
 
 var mult = 1;
 
@@ -18,7 +18,7 @@ function init() {
     0.01,
     100
   );
-  camera.position.z = 3;
+  camera.position.z = 5;
 
   scene = new THREE.Scene();
   renderer = new THREE.WebGLRenderer({ antialias: true });
@@ -27,9 +27,34 @@ function init() {
 }
 
 function initCube(x, y, z) {
+  // var face = [
+  // new THREE.Color("black"),
+  // new THREE.Color(0xff0000),
+  // new THREE.Color(0xff0000),
+  // new THREE.Color(0xff0000),
+  // new THREE.Color(0xff0000),
+  // new THREE.Color(0xff0000)
+  // ];
+  var geometry = new THREE.BoxGeometry(
+    boxDimensions,
+    boxDimensions,
+    boxDimensions
+  );
+  for (var i = 0; i < geometry.faces.length; i += 2) {
+  	randColor = Math.random() * 0xffffff
+    geometry.faces[i].color.setHex(randColor);
+    geometry.faces[i + 1].color.setHex(randColor);
+  }
   toPush = new THREE.Mesh(
-    new THREE.BoxGeometry(boxDimensions, boxDimensions, boxDimensions),
-    new THREE.MeshNormalMaterial()
+    geometry,
+    // new THREE.BoxGeometry(boxDimensions, boxDimensions, boxDimensions),
+    new THREE.MeshBasicMaterial({
+      color: 0xffffff,
+      vertexColors: THREE.FaceColors,
+    })
+    // new THREE.MeshBasicMaterial([{ color: "orange", color: "green" , color: "blue" }])
+    // new THREE.MeshToonMaterial()
+    // new THREE.MeshBasicMaterial()
   );
   toPush.position.x = x;
   toPush.position.y = y;
@@ -48,17 +73,30 @@ function loop() {
   // if (camera.position.z > 10 || camera.position.z < 3) mult = mult * -1;
 
   for (cube of cubes) {
-    // cube.rotation.x += 0.01;
-    // cube.rotation.y += 0.02;
+    cube.rotation.x += 0.01;
+    cube.rotation.y += 0.02;
   }
 
   renderer.render(scene, camera);
 }
 
-offset = boxDimensions
+offset = boxDimensions;
 init();
+
+// for (let i = -(window.innerWidth * 2); i < window.innerWidth * 2; i += offset) {
+// for (
+// let j = -(window.innerHeight * 2);
+// j < window.innerHeight * 2;
+// j += offset
+// ) {
+// console.log(i, j);
+// initCube(i * boxDimensions / offset, j * boxDimensions / offset, 0);
+// }
+// }
+
 initCube(0, 0, 0);
-initCube(offset, offset, 0, offset); 
+// alert(window.innerWidth / offset)
+initCube(offset, offset, 0, offset);
 initCube(-offset, offset, 0, -offset);
 initCube(offset, -offset, 0, offset);
 initCube(-offset, -offset, 0, -offset);
