08ba949837524c7c6b5119a40124b9ec108b947b
vandemjh <jack.vandem@gmail.com>
2020-04-09 20:40:19 -0400

    added light sources and rotation

--- app.js
+++ app.js
@@ -1,11 +1,32 @@
-var camera, scene, renderer;
+var camera, scene, renderer, lightSource;
 var geometry, material;
 var cubes = [];
+var cubeGroup = new THREE.Group();
 
 // const boxDimensions = 0.2;
 const boxDimensions = 2;
 
 var mult = 1;
+
+var dark = new THREE.Color(0x1c1c1c);
+var grey = new THREE.Color(0x808080);
+var light = new THREE.Color(0xececec);
+
+var colorChangeMultiplier = 0.0;
+var colorChange = [
+  -1 * colorChangeMultiplier,
+  -1 * colorChangeMultiplier,
+  -1 * colorChangeMultiplier,
+  -1 * colorChangeMultiplier,
+  1 * colorChangeMultiplier,
+  1 * colorChangeMultiplier,
+  1 * colorChangeMultiplier,
+  1 * colorChangeMultiplier,
+  -1 * colorChangeMultiplier,
+  -1 * colorChangeMultiplier,
+  -1 * colorChangeMultiplier,
+  -1 * colorChangeMultiplier,
+];
 
 window.onresize = function () {
   location.reload();
@@ -19,54 +40,57 @@ function init() {
     0.01,
     100
   );
-  camera.position.z = 50;
+  // camera.position.y -= 0.6
+  camera.position.z = 60;
+
+  lightSource = new THREE.PointLight(0xffffff, 1, 15);
+  lightSource.position.set(0, 5, 10);
+  lightSource1 = new THREE.PointLight(0xffffff, 1, 15);
+  lightSource1.position.set(5, 0, 10);
 
   scene = new THREE.Scene();
+  scene.add(lightSource);
+  scene.add(lightSource1);
+  scene.add(cubeGroup);
+
   renderer = new THREE.WebGLRenderer({ antialias: true });
   renderer.setSize(window.innerWidth, window.innerHeight);
   document.body.appendChild(renderer.domElement);
 }
 
 function initCube(x, y, z) {
-  // var face = [
-  // new THREE.Color("black"),
-  // new THREE.Color(0xff0000),
-  // new THREE.Color(0xff0000),
-  // new THREE.Color(0xff0000),
-  // new THREE.Color(0xff0000),
-  // new THREE.Color(0xff0000)
-  // ];
   var geometry = new THREE.BoxGeometry(
     boxDimensions,
     boxDimensions,
     boxDimensions
   );
-  // #1C1C1C - Dark
-  // #808080 - Grey
-  // #ECECEC - Light
-  geometry.faces[0].color.set("#ECECEC");
-  geometry.faces[1].color.set("#ECECEC");
-  geometry.faces[2].color.set("#808080");
-  geometry.faces[3].color.set("#808080");
-  geometry.faces[4].color.set("#1C1C1C");
-  geometry.faces[5].color.set("#1C1C1C");
-  geometry.faces[8].color.set("#ECECEC");
-  geometry.faces[9].color.set("#ECECEC");
-  geometry.faces[6].color.set("#1C1C1C");
-  geometry.faces[7].color.set("#1C1C1C");
-  geometry.faces[10].color.set("#808080");
-  geometry.faces[11].color.set("#808080");
+
+  // var dark = "#1C1C1C";
+  // var grey = "#808080";
+  // var light = "#ECECEC";
+  geometry.faces[0].color.set(light);
+  geometry.faces[1].color.set(light);
+  geometry.faces[2].color.set(light);
+  geometry.faces[3].color.set(light);
+  geometry.faces[4].color.set(dark);
+  geometry.faces[5].color.set(dark);
+  geometry.faces[6].color.set(dark);
+  geometry.faces[7].color.set(dark);
+  geometry.faces[8].color.set(grey);
+  geometry.faces[9].color.set(grey);
+  geometry.faces[10].color.set(grey);
+  geometry.faces[11].color.set(grey);
   toPush = new THREE.Mesh(
     geometry,
-    // new THREE.BoxGeometry(boxDimensions, boxDimensions, boxDimensions),
-    new THREE.MeshBasicMaterial({
-      color: 0xffffff,
-      vertexColors: THREE.FaceColors,
-    })
+    new THREE.MeshStandardMaterial()
+    // new THREE.MeshBasicMaterial({
+    // vertexColors: THREE.FaceColors,
+    // })
     // new THREE.MeshBasicMaterial([{ color: "orange", color: "green" , color: "blue" }])
     // new THREE.MeshToonMaterial()
     // new THREE.MeshBasicMaterial()
   );
+  // toPush.geometry.colorsNeedUpdate = true;
 
   toPush.rotateX(Math.PI / 4);
   toPush.rotateY(Math.PI / 4);
@@ -78,6 +102,8 @@ function initCube(x, y, z) {
   // toPush.rotation.x += .02;
 
   scene.add(toPush);
+  cubeGroup.add(toPush)
+  
   cubes.push(toPush);
 
   // var material = new THREE.LineBasicMaterial({ color: 0x0000ff });
@@ -89,17 +115,30 @@ function loop() {
   // camera.position.z += 0.01 * mult;
   // if (camera.position.z > 10 || camera.position.z < 3) mult = mult * -1;
 
-  for (cube of cubes) {
-    // cube.rotation.x += 0.01;
-    // cube.rotation.y += 0.02;
-    // cube.rotation.y += 0.02;
-    // console.log(cube.rotation.y)
-  }
-  camera.rotation.z += 0.002;
+  // for (cube of cubes) {
+  // for (let face = 0; face < cubes[0].geometry.faces.length; face += 2) {
+  // cubes[0].geometry.faces[0].color.r
+  // curColor = cube.geometry.faces[face].color.r;
+  // cube.geometry.faces[face].color.addScalar(colorChange[face]);
+  // cube.geometry.faces[face + 1].color.addScalar(colorChange[face + 1]);
+  // if (curColor > light.r || curColor < dark.r) {
+  // colorChange[face] *= -1;
+  // colorChange[face + 1] *= -1;
+  // }
+  // console.log(colorChange[face])
+  // }
+  // cube.geometry.colorsNeedUpdate = true;
+  // cube.rotation.x += 0.01;
+  // cube.rotation.y += 0.02;
+  // cube.rotation.y += 0.02;
+  // console.log(cube.rotation.y)
+  // }
+  // camera.rotation.z += 0.02;
+  cubeGroup.rotation.z += 0.01;
+
 
   renderer.render(scene, camera);
 }
-
 
 init();
 
@@ -111,23 +150,15 @@ initCube(boxDimensions - horiChange, boxDimensions + vertChange, 0);
 initCube(-boxDimensions + horiChange, boxDimensions + vertChange, 0);
 initCube(boxDimensions - horiChange, -boxDimensions - vertChange, 0);
 initCube(-boxDimensions + horiChange, -boxDimensions - vertChange, 0);
-initCube(boxDimensions + horiChange + 0.225, boxDimensions - vertChange - 1.61, 0);
+initCube(
+  boxDimensions + horiChange + 0.225,
+  boxDimensions - vertChange - 1.61,
+  0
+);
 initCube(
   -boxDimensions - vertChange - 0.425,
   -boxDimensions + horiChange + 1.41,
   0
 );
 
-// console.log(cubes[0].
-
-// cubeNum = 4;
-// for (let i = -cubeNum; i < cubeNum; i += boxDimensions) {
-// for (let j = -cubeNum; j < cubeNum; j += boxDimensions) {
-// initCube(i, j, 0);
-// }
-// }
-
-// console.log(cubes[0].position.x);
-// cubes[0].position.x += 100;
-// console.log(cubes[0].position.x);
 loop();
