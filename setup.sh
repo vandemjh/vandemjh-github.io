12b34fffcc9cc0cee7b99aba91267de3296a15b5
Jack Vandemeulebroecke <38433983+vandemjh@users.noreply.github.com>
2020-04-22 23:03:09 +0000

    Delete setup.sh
    Moving to its own repo

--- setup.sh
+++
@@ -1,6 +0,0 @@
-echo ===== Cloning repo =====
-echo ".cfg" >> .gitignore
-git clone --bare https://github.com/vandemjh/dotfiles.git $HOME/.cfg
-config checkout
-config config --local status.showUntrackedFiles no
-rm .gitignore
