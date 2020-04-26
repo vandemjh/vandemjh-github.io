d2140a120bf85d877c5c3a64ad03da70bed3040d
vandemjh <jack.vandem@gmail.com>
2020-04-26 02:53:34 -0400

    added another js scene

--- randomize.js
+++ randomize.js
@@ -1,6 +1,9 @@
-var jsfiles = ["app.js"];
+var jsfiles = [
+	// "app.js",
+	"multiple.js"
+];
 
-window.onload = function () {
+window.onload = () => {
     var script = document.createElement("script");
     script.src = "js/" + jsfiles[Math.floor(Math.random() * jsfiles.length)];
     document.getElementsByTagName("footer")[0].appendChild(script);
