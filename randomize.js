7b64e399d1fd9e9304d04fecb754c651cca6e0bd
vandemjh <jack.vandem@gmail.com>
2020-05-01 22:53:02 -0400

    updated to allow people to link to certain js scenes

--- randomize.js
+++ randomize.js
@@ -1,12 +1,22 @@
-var jsfiles = [
-	"app.js",
-	"multiple.js",
-	"click.js",
+const jsFiles = [
+	"app.js", 
+	"multiple.js", 
+	"click.js", 
 	"outrun.js"
-];
+	];
 
 window.onload = () => {
     var script = document.createElement("script");
-    script.src = "js/" + jsfiles[Math.floor(Math.random() * jsfiles.length)];
+    var urlParams = new URLSearchParams(window.location.search);
+    if (
+        urlParams.has("js") &&
+        jsFiles.some((file) => file.includes(urlParams.get("js")))
+    ) {
+        script.src =
+            "js/" +
+            jsFiles.find((file) => file.includes(urlParams.get("js")));
+    } else
+        script.src =
+            "js/" + jsFiles[Math.floor(Math.random() * jsFiles.length)];
     document.getElementsByTagName("footer")[0].appendChild(script);
 };
