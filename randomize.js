6889c0abac5f10df6db98dce3acc3d8e1d935daf
vandemjh <jack.vandem@gmail.com>
2020-06-03 14:35:44 -0400

    fixed randomize bug involving not loading requirements for random files

--- randomize.js
+++ randomize.js
@@ -1,11 +1,12 @@
+//JSON structured as "name-of-file": ["array-of-requirements"]
 const jsFiles = {
-    "app.js":[],
-    "multiple.js":[],
-    "click.js":[],
-    "outrun.js":[],
-    "penrose.js":[],
-    "paradise.js":[],
-    // "sphere.js":["onresize","init"]
+    "app.js": [],
+    "multiple.js": [],
+    "click.js": [],
+    "outrun.js": [],
+    "penrose.js": [],
+    "paradise.js": [],
+    "global.js": ["onresize", "init"],
 };
 
 var debug = false;
@@ -15,43 +16,55 @@ window.onload = () => {
     var url = window.location.href;
     var split = url.split("?");
     var param = split[split.length - 1];
-    // var urlParams = new URLSearchParams(window.location.search);
-    if (param != undefined && Object.keys(jsFiles).some((file) => file.includes(param))) {
-    	var file = Object.keys(jsFiles).find((file) => file.includes(param));
-    	var loaded = 0;
-    	var date;
-    	if (debug) date = Date.now();
-    	
-    	jsFiles[file].forEach((file) => {
-    	    var script = document.createElement("script");
-    	    script.src = "js/utils/" + file + ".js";
-    	    script.onload = () => {
-    	        loaded++;
-    	        if (debug) console.log("Loaded " + file);
-    	    };
-    	    document.getElementsByTagName("footer")[0].appendChild(script);
-    	});
-    	
-    	var wait = () => {
-    	    if (loaded != jsFiles[file].length) {
-    	        setTimeout(wait, 1);
-    	    } else {
-    	        if (debug)
-    	            console.log(
-    	                "Loaded " +
-    	                    loaded +
-    	                    " of " +
-    	                    jsFiles[file].length +
-    	                    " requirements in " +
-    	                    (Date.now() - date) +
-    	                    " milliseconds."
-    	            );
-    	    }
-    	}
-    	wait();
+
+    let loadRequirements = (file) => {
+        var loaded = 0;
+        var date;
+        if (debug) date = Date.now();
+
+        jsFiles[file].forEach((file) => {
+            var script = document.createElement("script");
+            script.src = "js/utils/" + file + ".js";
+            script.onload = () => {
+                loaded++;
+                if (debug) console.log("Loaded " + file);
+            };
+            document.getElementsByTagName("footer")[0].appendChild(script);
+        });
+
+        var wait = () => {
+            if (loaded != jsFiles[file].length) {
+                setTimeout(wait, 1);
+            } else {
+                if (debug)
+                    console.log(
+                        "Loaded " +
+                            loaded +
+                            " of " +
+                            jsFiles[file].length +
+                            " requirements in " +
+                            (Date.now() - date) +
+                            " milliseconds."
+                    );
+            }
+        };
+        wait();
         script.src = "js/" + file;
-    } else
-        script.src =
-            "js/" + Object.keys(jsFiles)[Math.floor(Math.random() * Object.keys(jsFiles).length)];
+    };
+
+    if (
+        param != undefined &&
+        Object.keys(jsFiles).some((file) => file.includes(param))
+    ) {
+        loadRequirements(
+            Object.keys(jsFiles).find((file) => file.includes(param))
+        );
+    } else {
+        var source = Object.keys(jsFiles)[
+            Math.floor(Math.random() * Object.keys(jsFiles).length)
+        ];
+        script.src = "js/" + source;
+        loadRequirements(source);
+    }
     document.getElementsByTagName("footer")[0].appendChild(script);
 };
