832de0479c0a706fbc29ddf32f056b3b4d8f6b9e
vandemjh <jack.vandem@gmail.com>
2020-06-28 21:08:01 -0400

    added module support and search terms for querying

--- init.js
+++ init.js
@@ -1,4 +1,5 @@
-//JSON structured as "name-of-file": ["array-of-requirements"]
+// JSON structured as "name-of-file": ["possible", "search", "terms"]
+// TODO put search terms for each file here
 const jsFiles = {
     "app.js": [],
     "multiple.js": [],
@@ -6,65 +7,28 @@ const jsFiles = {
     "outrun.js": [],
     "penrose.js": [],
     "paradise.js": [],
-    "global.js": ["onresize", "init", "colors"],
+    "global.js": [],
 };
 
 var debug = false;
 
 window.onload = () => {
     var script = document.createElement("script");
+    script.type = "module"
     var url = window.location.href;
     var split = url.split("?");
     var param = split[split.length - 1];
-
-    let loadRequirements = (file) => {
-        var loaded = 0;
-        var date;
-        if (debug) date = Date.now();
-
-        jsFiles[file].forEach((file) => {
-            var script = document.createElement("script");
-            script.src = "js/utils/" + file + ".js";
-            script.onload = () => {
-                loaded++;
-                if (debug) console.log("Loaded " + file);
-            };
-            document.getElementsByTagName("footer")[0].appendChild(script);
-        });
-
-        var wait = () => {
-            if (loaded != jsFiles[file].length) {
-                setTimeout(wait, 1);
-            } else {
-                if (debug)
-                    console.log(
-                        "Loaded " +
-                            loaded +
-                            " of " +
-                            jsFiles[file].length +
-                            " requirements in " +
-                            (Date.now() - date) +
-                            " milliseconds."
-                    );
-            }
-        };
-        wait();
-        script.src = "js/" + file;
-    };
 
     if (
         param != undefined &&
         Object.keys(jsFiles).some((file) => file.includes(param))
     ) {
-        loadRequirements(
-            Object.keys(jsFiles).find((file) => file.includes(param))
-        );
+    	script.src = "js/" + Object.keys(jsFiles).find((file) => file.includes(param))
     } else {
         var source = Object.keys(jsFiles)[
             Math.floor(Math.random() * Object.keys(jsFiles).length)
         ];
         script.src = "js/" + source;
-        loadRequirements(source);
     }
     document.getElementsByTagName("footer")[0].appendChild(script);
 };
