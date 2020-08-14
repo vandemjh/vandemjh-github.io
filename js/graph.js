e8bfa8eac5353b9337ab09e2951b5f39a20c3643
vandemjh <jack.vandem@gmail.com>
2020-08-14 19:51:07 -0400

    rough draft, kinda cool

--- js/graph.js
+++ js/graph.js
@@ -25,14 +25,8 @@ function initGraph() {
       side: THREE.DoubleSide,
     })
   );
-  const edge = new THREE.Mesh(
-    new THREE.BoxGeometry(1, 0.02, 0.02),
-    new THREE.MeshBasicMaterial({
-      color: Colors.LIMEGREEN,
-      side: THREE.DoubleSide,
-    })
-  );
-  for (let i = 0; i < 2; i++) {
+  var points = [];
+  for (let i = 0; i < 10; i++) {
     var clone = node.clone();
     clone.position.set(
       randomDecimals(-1, 1),
@@ -45,42 +39,24 @@ function initGraph() {
     return graph.children[random(0, graph.children.length - 1)];
   };
 
-  var clone = edge.clone();
-  let child1 = graph.children[0];
-  let child2 = graph.children[1];
-  console.log(child1.position.distanceTo(child2.position));
-  clone.position.set(child1.position.x, child1.position.y, child1.position.z);
-  clone.geometry.scale(child1.position.distanceTo(child2.position), 1, 1);
-  // clone.geometry.scale(child.position.distanceTo(child2));
-  // clone.position.set(child.position.x, child.position.y, child.position.z);
-  var lookAt = new THREE.Vector3(
-    child2.position.x * -1,
-    child2.position.y,
-    child2.position.z
+  graph.children.forEach(() => {
+    graph.children.forEach((child) => {
+      var child = graph.getRandomChild();
+      points.push(
+        new THREE.Vector3(child.position.x, child.position.y, child.position.z)
+      );
+    });
+  });
+  const edge = new THREE.Line(
+    new THREE.Geometry().setFromPoints(points),
+    new THREE.LineBasicMaterial({ color: Colors.LIMEGREEN })
   );
-  // lookAt.x = lookAt.x * -1;
-  // lookAt.y = lookAt.y * -1;
-  // lookAt.z = lookAt.z * -1;
-  clone.lookAt(lookAt);
-
-  graph.add(clone);
-
-  // graph.children.forEach((child) => {
-  //   var clone = edge.clone();
-  //   let randomChild = graph.getRandomChild();
-  //   console.log(child.position.distanceTo(randomChild.position));
-  //   clone.geometry.scale(
-  //     child.position.distanceTo(randomChild.position)
-  //   )
-  //   // clone.position.set(child.position.x, child.position.y, child.position.z);
-  //   // clone.lookAt(0, 0, 0);
-
-  //   graph.add(clone);
-  // });
+  graph.add(edge);
 
   graph.update = () => {
-    graph.rotation.x += 0.04;
-    // graph.rotation.z += 0.004;
+    graph.rotation.x += 0.004;
+    graph.rotation.y += 0.004;
+    graph.rotation.z += 0.004;
   };
 
   scene.add(graph);
