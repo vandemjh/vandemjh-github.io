bcbffdb3a12846ea960eab16de48f35ccdcf62da
vandemjh <jack.vandem@gmail.com>
2020-08-24 12:15:53 -0400

    added session storage so users do not get the same design twice in a row

--- init.js
+++ init.js
@@ -1,36 +1,38 @@
 // JSON structured as "name-of-file": ["possible", "search", "terms"]
 // TODO put search terms for each file here
 const jsFiles = {
-    "app.js": [],
-    "multiple.js": [],
-    "click.js": [],
-    "outrun.js": [],
-    "penrose.js": [],
-    "paradise.js": [],
-    "global.js": [],
-    "graph.js": []
+  'app.js': [],
+  'multiple.js': [],
+  'click.js': [],
+  'outrun.js': [],
+  'penrose.js': [],
+  'paradise.js': [],
+  'global.js': [],
+  'graph.js': [],
 };
 
 var debug = false;
 
 window.onload = () => {
-    var script = document.createElement("script");
-    script.type = "module";
-    var url = window.location.href;
-    var split = url.split("?");
-    var param = split[split.length - 1];
+  var script = document.createElement('script');
+  script.type = 'module';
+  var url = window.location.href;
+  var split = url.split('?');
+  var param = split[split.length - 1];
+  var visited = Object.keys(sessionStorage);
+  var files = Object.keys(jsFiles);
+  var unvisited = files.filter((v) => !visited.includes(v));
 
-    if (
-        param != undefined &&
-        Object.keys(jsFiles).some((file) => file.includes(param))
-    ) {
-        script.src =
-            "js/" + Object.keys(jsFiles).find((file) => file.includes(param));
-    } else {
-        var source = Object.keys(jsFiles)[
-            Math.floor(Math.random() * Object.keys(jsFiles).length)
-        ];
-        script.src = "js/" + source;
+  if (param != undefined && files.some((file) => file.includes(param))) {
+    script.src = 'js/' + files.find((file) => file.includes(param));
+  } else {
+    if (unvisited.length == 0) {
+      sessionStorage.clear();
+      unvisited = files;
     }
-    document.getElementsByTagName("footer")[0].appendChild(script);
+    var source = unvisited[Math.floor(Math.random() * unvisited.length)];
+    script.src = 'js/' + source;
+    sessionStorage.setItem(source, 'visited');
+  }
+  document.getElementsByTagName('footer')[0].appendChild(script);
 };
